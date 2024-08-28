cb3bc026130b8b74febca9f6685972b9df40810f
Tiago Krebs <tiago.krebs@azion.com>
2024-08-28 14:40:10 -0300

    error

--- weather-cli.py
+++ weather-cli.py
@@ -1,4 +1,6 @@
 import requests
+import logging
+logger = logging.getLogger(__name__)
 
 def feels_like(temp, humidity):
     return temp - ((0.55 - 0.0055 * humidity) * (temp - 14.5))
@@ -6,9 +8,12 @@ def feels_like(temp, humidity):
 def celcius_to_kelvin(temp):
     return temp + 273.15
 
+FORMAT = '%(asctime)s %(message)s'
+logging.basicConfig(format=FORMAT, filename='weather-cli.log', level=logging.INFO)
+
 LAT = -30.03283000
 LONG = -51.23019000
-API_KEY = "3b6f2edd765dd6fc88169a6d75586b96"
+API_KEY = ""
 EXCLUDE = "minutely,hourly,daily,alerts"
 
 url = f"https://api.openweathermap.org/data/3.0/onecall" 
@@ -19,20 +24,39 @@ try:
     r = requests.get(url, headers=headers, params=payload)
     # print(r.headers['Content-Type'])
     # print(r.json())
+    r.raise_for_status()
 
-    fl = feels_like(r.json()['current']['temp'], r.json()['current']['humidity'])
+    data = r.json()
+
+    fl = feels_like(data['current']['temp'], data['current']['humidity'])
     flk = celcius_to_kelvin(fl)
 
-    print('Temperature: %.2f' % r.json()['current']['temp'])
+    print('Temperature: %.2f' % data['current']['temp'])
     print('Feels like (C): %.2f' % fl)
     print('Feels like (K): %.2f' % flk)
-# except Exception as e:
-#     print(e)
+
+except requests.exceptions.InvalidJSONError as e:
+    print(f"Invalid JSON: {e}")
+    logger.error(f"Invalid JSON: {e}")
+except requests.exceptions.ConnectionError as e:
+    print(f"Connection error: {e}")
+    logger.error(f"Connection error: {e}")
 except requests.exceptions.RequestException as e:
     print(f"Request error: {e}")
+    logger.error(f"Request error: {e}")
 except requests.exceptions.HTTPError as e:
-    print(f"HTTP error: {e}")
+    status_code = e.response.status_code
+    if status_code >= 401 and status_code <= 403:
+        print("Authentication error: Invalid API key or insufficient permissions.")
+    elif status_code == 429:
+        print("Too many requests error: Slow down.")
+    else:
+        print(f"HTTP error: {e}")
+    logger.error(f"Authentication error: {status_code}")
 except Exception as e:
-    print(f"An error occurred: {e}")
+    print(f"An error occurred: {e.with_traceback()}")
+    logger.error(f"An error occurred: {e.with_traceback()}")
 # finally:
 #     print('finally')
+
+print("The end.s")
